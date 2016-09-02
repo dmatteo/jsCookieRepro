8cd928d25bc14618562e7a6599dd980b19985feb
Domenico Matteo <matteo.domenico@gmail.com>
2016-09-01 23:39:26 -0400

    non transpiled example, still JSDOM 9.4.2

--- test.js
+++ test.js
@@ -1,16 +1,23 @@
-const Cookies = require('js-cookie');
-import jsdomify from 'jsdomify';
-import expect from 'unexpected';
+var Cookies = require('js-cookie');
+var jsdomify = require('jsdomify').default;
+var expect = require('unexpected');
 
-describe('JsCookie Integration', () => {
-  before(() => {
+describe('JsCookie Integration', function() {
+  before(function() {
     jsdomify.create();
   });
 
-  it('should create and retrieve cookies', () => {
+  it('should create and retrieve cookies', function() {
     Cookies.set('c', '1337');
-    const value = Cookies.get('c');
-    expect(value, 'to be', '1337')
+    var value = Cookies.get('c');
+
+    expect(value, 'to be', '1337');
+  });
+
+  it('should get undefined when retrieving non existing cookie', function() {
+    var nonExisting = Cookies.get('yolo');
+
+    expect(nonExisting, 'to be undefined');
   });
 
 });
